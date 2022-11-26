9822b3eeded71277a6ba462b8bbcbc64eb8c2af9
diegod21 <108184501+diegod21@users.noreply.github.com>
2022-11-26 09:49:31 -0300

    começando do zero

--- AULA12/ex0011.js
+++
@@ -1,8 +0,0 @@
-var idade = 22
-if(idade < 16){
-    console.log('não vota')
-} 
-else if(idade < 18){
-        console.log('voto opcional')
-    }
-
--- AULA15/ex00.js
+++
@@ -1,6 +0,0 @@
-let num = [5 , 8 , 9, 21] 
-
-console.log(`o vetor tem ${num.length} posições`)
-console.log(`o numero em ordem é ${num.sort()}`)
-
-//buscar o valor no vetor {num.indexOf(21)}
